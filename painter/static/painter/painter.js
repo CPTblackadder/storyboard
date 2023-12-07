7d98bb83554345f2eb22f7fe82bb5c95d7f42ca3
CPTblackadder <rob@robvandyk.com>
2023-12-07 18:26:42 +0000

    Minor cosmetic adjustments

--- painter/static/painter/painter.js
+++ painter/static/painter/painter.js
@@ -6,7 +6,7 @@ var body = document.getElementsByTagName("body")[0];
 var ctx = canvas.getContext('2d');
 var linesArray = [];
 currentSize = 5;
-var currentColor = "rgb(200,20,100)";
+var currentColor = "rgb(200, 20, 100))";
 var currentBg = "white";
 
 // INITIAL LAUNCH
